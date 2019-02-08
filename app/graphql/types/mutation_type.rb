da262aac97c55ee3a380384f7eab8bbf59cf38ad
Thomas Paresi <thomas.paresi@thoughtbot.com>
2019-02-08 14:03:13 -0600

    Create review mutation
    Signed-off-by: Annie Lydens <annie@thoughtbot.com>

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,7 @@ module Types
   class MutationType < BaseObject
     field :create_user, mutation: Mutations::CreateUser
     field :sign_in_user, mutation: Mutations::SignInUser
+
+    field :create_review, mutation: Mutations::CreateReview
   end
 end
--- app/graphql/types/review_type.rb
+++ app/graphql/types/review_type.rb
@@ -2,6 +2,6 @@ class Types::ReviewType < Types::BaseObject
   field :id, ID, null: false
   field :comment, String, null: true
   field :rating, Float, null: false
-  field :user, [Types::UserType], null: false
-  field :movie, [Types::MovieType], null: false
+  field :user, Types::UserType, null: false
+  field :movie, Types::MovieType, null: false
 end
